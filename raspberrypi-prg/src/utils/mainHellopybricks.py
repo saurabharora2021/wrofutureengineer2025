06656f4c64300bea8aaa2fcef7290d972998c06d
Kanak Arora <kanakarora2009@gmail.com>
2025-05-14 01:48:51 +0530

    test pybricks changes.

--- raspberrypi-prg/src/utils/mainHellopybricks.py
+++ raspberrypi-prg/src/utils/mainHellopybricks.py
@@ -1,16 +1,17 @@
-from pybricksdev.usb import PybricksHubUSB
+from pybricksdev.connections.pybricks import PybricksHubUSB
+from pybricksdev.connections.pybricks import PybricksHub
 
 # Initialize the USB connection
-usb = PybricksHubUSB()
+usb = PybricksHubUSB(PybricksHub())
 
 # Connect to the hub
 usb.connect()
 
 # Send a command to the hub
-usb.write(b"Hello Hub!")
+usb.write_line(b"Hello Hub!")
 
 # Read a response from the hub
-response = usb.read()
+response = usb.read_line()
 print(f"Response from hub: {response}")
 
 # Disconnect the USB connection

774d926f7a204060ea6e99a74047fd3c976b0be5
Kanak Arora <kanakarora2009@gmail.com>
2025-05-14 02:15:59 +0530

    test 3 using advance pybricksusb

--- raspberrypi-prg/src/utils/mainHellopybricks.py
+++ raspberrypi-prg/src/utils/mainHellopybricks.py
@@ -1,17 +1,51 @@
 from pybricksdev.connections.pybricks import PybricksHubUSB
-from pybricksdev.connections.pybricks import PybricksHub
+from usb.core import find as find_usb
+import asyncio
+from pybricksdev.usb import (
+                LEGO_USB_VID,
+                MINDSTORMS_INVENTOR_USB_PID,
+                SPIKE_ESSENTIAL_USB_PID,
+                SPIKE_PRIME_USB_PID,
+            )
+
+import usb.backend.libusb1
+backend = usb.backend.libusb1.get_backend()
+if backend is None:
+    print("No libusb backend found!")
+else:
+    print("libusb backend is available.")
+    
+
+def is_pybricks_usb(dev):
+                return (
+                    (dev.idVendor == LEGO_USB_VID)
+                    and (
+                        dev.idProduct
+                        in [
+                            SPIKE_PRIME_USB_PID,
+                            SPIKE_ESSENTIAL_USB_PID,
+                            MINDSTORMS_INVENTOR_USB_PID,
+                        ]
+                    )
+                    and dev.product.endswith("Pybricks")
+                )
+
+#Find the USB device
+device_or_address = find_usb(custom_match=is_pybricks_usb)
+if device_or_address is None:
+    raise RuntimeError("No Pybricks USB device found")
 
 # Initialize the USB connection
-usb = PybricksHubUSB(PybricksHub())
+usb = PybricksHubUSB(PybricksHub(device_or_address))
 
 
-async def hello_world():
-    
+def hello_world(loop: asyncio.AbstractEventLoop):
+
     # Connect to the hub
-    usb.connect()
+    loop.run_until_complete(usb.connect())
 
     # Send a command to the hub
-    usb.write_line(b"Hello Hub!")
+    loop.run_until_complete(usb.write_line(b"Hello Hub!"))
 
     # Read a response from the hub
     response = usb.read_line()
@@ -20,8 +54,11 @@ async def hello_world():
     # Disconnect the USB connection
     usb.disconnect()
 
+
+
+
 if __name__ == "__main__":
-    import asyncio
+    
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(hello_world())
+    hello_world(loop)
     loop.close()

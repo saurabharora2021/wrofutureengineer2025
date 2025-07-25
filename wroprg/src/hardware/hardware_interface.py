143e582de009051a764cbd540c9f896b6f76f171
Kanak Arora <kanakarora2009@gmail.com>
2025-07-25 18:39:03 +0530

    hide all hardware implementation inside Interface.

--- wroprg/src/hardware/hardware_interface.py
+++ wroprg/src/hardware/hardware_interface.py
@@ -14,13 +14,13 @@ class HardwareInterface(ShutdownInterface):
     """
 
     def __init__(self):
-        self.rpi = RpiInterface()
-        self.lego_drive_base: BuildHatDriveBase | None = None
+        self._rpi = RpiInterface()
+        self._lego_drive_base: BuildHatDriveBase | None = None
 
     def full_initialization(self) -> None:
         """Initialize all hardware components."""
         try:
-            self.lego_drive_base = BuildHatDriveBase(front_motor_port='D', back_motor_port='A',
+            self._lego_drive_base = BuildHatDriveBase(front_motor_port='D', back_motor_port='A',
                                                bottom_color_sensor_port='C',
                                                front_distance_sensor_port='B')
         except Exception as e:
@@ -31,47 +31,47 @@ class HardwareInterface(ShutdownInterface):
     # --- Raspberry Pi Interface Methods ---
     def buzzer_beep(self, timer: int = 1) -> None:
         """Turn on the buzzer."""
-        self.rpi.buzzer_beep(timer)
+        self._rpi.buzzer_beep(timer)
 
     def led1_green(self) -> None:
         """Turn on the LED1 green."""
-        self.rpi.led1_green()
+        self._rpi.led1_green()
 
     def led1_red(self) -> None:
         """Turn on the LED1 red."""
-        self.rpi.led1_red()
+        self._rpi.led1_red()
 
     def led1_blue(self) -> None:
         """Turn on the LED1 blue."""
-        self.rpi.led1_blue()
+        self._rpi.led1_blue()
 
     def led1_white(self) -> None:
         """Turn on the LED1 white."""
-        self.rpi.led1_white()
+        self._rpi.led1_white()
 
     def led1_off(self) -> None:
         """Turn off the LED1."""
-        self.rpi.led1_off()
+        self._rpi.led1_off()
 
     def wait_for_action(self) -> None:
         """Wait for the action button to be pressed."""
-        self.rpi.wait_for_action()
+        self._rpi.wait_for_action()
 
     def get_right_distance(self) -> float:
         """Get the distance from the right distance sensor."""
-        return self.rpi.get_right_distance()
+        return self._rpi.get_right_distance()
 
     def get_right_distance_max(self) -> float:
         """Get the maximum distance from the right distance sensor."""
-        return self.rpi.get_right_distance_max()
+        return self._rpi.get_right_distance_max()
 
     def get_left_distance(self) -> float:
         """Get the distance from the left distance sensor."""
-        return self.rpi.get_left_distance()
+        return self._rpi.get_left_distance()
 
     def get_left_distance_max(self) -> float:
         """Get the maximum distance from the left distance sensor."""
-        return self.rpi.get_left_distance_max()
+        return self._rpi.get_left_distance_max()
 
     def display_message(self, message: str, forceflush: bool = False) -> None:
         """
@@ -79,20 +79,20 @@ class HardwareInterface(ShutdownInterface):
 
         Only the last 5 messages are shown on the display.
         """
-        self.rpi.display_message(message, forceflush)
+        self._rpi.display_message(message, forceflush)
 
     def force_flush_messages(self) -> None:
         """Force flush the messages on the OLED screen."""
-        self.rpi.force_flush_messages()
+        self._rpi.force_flush_messages()
 
     def shutdown(self) -> None:
-        self.lego_drive_base.shutdown()
-        self.rpi.shutdown()
+        self._lego_drive_base.shutdown()
+        self._rpi.shutdown()
 
     # --- LEGO Driver Methods ---
     def drive_forward(self, speed: float) -> None:
         """Run the drive base forward at the specified speed."""
-        self.lego_drive_base.run_front(speed)
+        self._lego_drive_base.run_front(speed)
 
     def turn_steering(self, degrees: float,steering_speed:float=10) -> None:
         """
@@ -100,8 +100,8 @@ class HardwareInterface(ShutdownInterface):
         Positive degrees turn right, negative turn left.
         Limits steering to +/-38 degrees.
         """
-        self.lego_drive_base.turn_steering(degrees, steering_speed)
+        self._lego_drive_base.turn_steering(degrees, steering_speed)
 
     def drive_stop(self) -> None:
         """Stop the drive base."""
-        self.lego_drive_base.stop()
+        self._lego_drive_base.stop()
